2cdce84606d470ac57c3ebc1a4c6949dea2d88d6
Anisha <anisha.0612@live.com>
2021-02-10 16:14:57 -0500

    project files

---
+++ BasicReactScaffold/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  render() {
+    return <div>React App with Webpack</div>;
+  }
+}
+export default App;
---
+++ BasicReactScaffold/webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, "src", "index.js"),
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "build", "bundle.js"),
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.html?$/,
+        exclude: /node_modules/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: "file-loader?name=images/[name].[ext]",
+      },
+    ],
+  },
+
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
+};
